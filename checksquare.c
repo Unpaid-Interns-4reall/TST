c1d92e516ad4f2e2f2c7a3c4bdcdb3b447399752
Mohamad-Nahhal <mzn03@mail.aub.edu>
2025-03-09 17:14:29 +0200

    Updated checksquare function to new version

--- checksquare.c
+++ checksquare.c
@@ -1,49 +1,68 @@
-void checksquare(int row, int col, char player,
-                 int horizontaledges[ROWS + 1][COLUMNS - 1],
-                 int verticaledges[ROWS - 1][COLUMNS + 1],
-                 char boxes[ROWS][COLUMNS])
-{
+#include <stdio.h>
 
+#define ROWS 4
+#define COLUMNS 5
+
+// 1) checksquare function
+void checksquare(int row, int col, char player, 
+                 int hor[5][6],
+                 int ver[5][6],
+                 char boxes[4][5])
+{
     int completed = 0;
 
-    // check if it is horizontal
-    if (row < ROWS + 1 && col < COLUMNS - 1 && horizontaledges[row][col] == 1)
+    // If this was a horizontal edge
+    if ((row >= 0 && row < 5) && (col >= 0 && col < 5) && hor[row][col] == 1)
     {
-
-        // check box below
-        if (row < ROWS && horizontaledges[row + 1][col] && verticaledges[row][col] && verticaledges[row][col + 1])
+        // Check box below
+        if (row < 4
+            && boxes[row][col] == 0  
+            && hor[row+1][col] == 1 
+            && ver[row][col] == 1 
+            && ver[row][col+1] == 1)
+            {
+                boxes[row][col] = player;
+                completed = 1;
+            }
+            // Check box above
+        if (row > 0 
+            && boxes[row-1][col] == 0  
+            && hor[row-1][col] == 1
+            && ver[row-1][col] == 1
+            && ver[row-1][col+1] == 1)
+            {
+                boxes[row-1][col] = player;
+                completed = 1;
+            }
+        }
+        
+        // If this was a vertical edge
+        if (row >= 0 && row < 4 && col >= 0 && col < 6 && ver[row][col] == 1)
+        {
+            // Check box to the right
+        if (col < 5
+            && boxes[row][col] == 0  
+            && ver[row][col+1] == 1
+            && hor[row][col] == 1
+            && hor[row+1][col] == 1)
         {
             boxes[row][col] = player;
             completed = 1;
         }
+        // Check box to the left
+        if (col > 0
+            && boxes[row][col-1] == 0
+            && ver[row][col-1] == 1
+            && hor[row][col-1] == 1
+            && hor[row+1][col-1] == 1)
+        {
+            boxes[row][col-1] = player;
+            completed = 1;
+        }
+    }
 
-        // check box above
-        if (row > 0 && horizontaledges[row - 1][col] && verticaledges[row - 1][col] && verticaledges[row - 1][col + 1])
-        {
-            boxes[row - 1][col] = player;
-            completed = 1;
-        }
-    }
-    // check if it is vertical
-    if (row < ROWS - 1 && col < COLUMNS + 1 && verticaledges[row][col] == 1)
+    if (completed == 1)
     {
-
-        // check box to the right
-        if (col < COLUMNS - 1 && verticaledges[row][col + 1] && horizontaledges[row][col + 1] && horizontaledges[row + 1][col])
-        {
-            boxes[row][col] = player;
-            completed = 1;
-        }
-        // check box to the left
-        if (col > 0 && verticaledges[row][col - 1] && horizontaledges[row][col - 1] && horizontaledges[row + 1][col - 1])
-        {
-            boxes[row][col - 1] = player;
-            completed = 1;
-        }
+        printf("Player %c completed a square!\n", player);
     }
-    if (completed)
-    {
-        printf("Player %c completed a square!", player);
-    }
-}
 }
