914fd55f53324267001bc03b080366cac39e2736
Mohamad-HK22 <myk26@mail.aub.edu>
2025-04-15 16:06:51 +0300

    sub 3

--- game_bots.c
+++ game_bots.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "game_bots.h"
 #include "game_logic.h"
 
@@ -29,80 +30,179 @@ int easy_bot(int hor[5][6], int ver[5][6], char boxes[4][5], char bot) {
             valid_move = 1;
         }
     }
-
+    
     if (isHorizontal) {
         printf("Bot chose: Horizontal at (%d, %d)\n", row, col);
     } else {
         printf("Bot chose: Vertical at (%d, %d)\n", row, col);
     }
-
+    
     return newScore - oldScore;
 }
 
-// Medium bot: first checks for a move that completes a box.
-// If none is found, it falls back to a random move.
-int medium_bot(int hor[5][6], int ver[5][6], char boxes[4][5], char bot) {
-    int tempHor[5][6], tempVer[5][6];
-    char tempBoxes[4][5];
-    int oldScore, newScore;
-    
-    // Check horizontal moves.
-    for (int i = 0; i < 5; i++) {
-        for (int j = 0; j < 5; j++) {
-            if (hor[i][j] == 0) {
-                for (int a = 0; a < 5; a++) {
-                    for (int b = 0; b < 6; b++) {
-                        tempHor[a][b] = hor[a][b];
-                        tempVer[a][b] = ver[a][b];
-                    }
-                }
-                for (int a = 0; a < 4; a++) {
-                    for (int b = 0; b < 5; b++) {
-                        tempBoxes[a][b] = boxes[a][b];
-                    }
-                }
-                oldScore = countBoxes(bot, tempBoxes);
-                tempHor[i][j] = 1;
-                checksquare(i, j, bot, tempHor, tempVer, tempBoxes);
-                newScore = countBoxes(bot, tempBoxes);
-                if (newScore - oldScore > 0) {
-                    hor[i][j] = 1;
-                    checksquare(i, j, bot, hor, ver, boxes);
-                    printf("Bot chose: Horizontal at (%d, %d)\n", i, j);
-                    return newScore - oldScore;
-                }
-            }
-        }
-    }
-    
-    // Check vertical moves.
-    for (int i = 0; i < 4; i++) {
-        for (int j = 0; j < 6; j++) {
-            if (ver[i][j] == 0) {
-                for (int a = 0; a < 5; a++) {
-                    for (int b = 0; b < 6; b++) {
-                        tempHor[a][b] = hor[a][b];
-                        tempVer[a][b] = ver[a][b];
-                    }
-                }
-                for (int a = 0; a < 4; a++) {
-                    for (int b = 0; b < 5; b++) {
-                        tempBoxes[a][b] = boxes[a][b];
-                    }
-                }
-                oldScore = countBoxes(bot, tempBoxes);
-                tempVer[i][j] = 1;
-                checksquare(i, j, bot, tempHor, tempVer, tempBoxes);
-                newScore = countBoxes(bot, tempBoxes);
-                if (newScore - oldScore > 0) {
-                    ver[i][j] = 1;
-                    checksquare(i, j, bot, hor, ver, boxes);
-                    printf("Bot chose: Vertical at (%d, %d)\n", i, j);
-                    return newScore - oldScore;
-                }
-            }
-        }
-    }
+// Helper function to copy board state.
+void copy_state(int srcHor[5][6], int srcVer[5][6], char srcBoxes[4][5],
+                int destHor[5][6], int destVer[5][6], char destBoxes[4][5]) {
+    memcpy(destHor, srcHor, sizeof(int) * 5 * 6);
+    memcpy(destVer, srcVer, sizeof(int) * 5 * 6);
+    memcpy(destBoxes, srcBoxes, sizeof(char) * 4 * 5);
+}
 
-    return easy_bot(hor, ver, boxes, bot);
+// Recursive function that simulates extra moves from a given state.
+// Returns the maximum additional boxes that can be captured.
+int simulate_chain(int hor[5][6], int ver[5][6], char boxes[4][5], char bot) {
+    int maxChain = 0;
+    int moveFound = 0;
+    
+    // Try every possible move.
+    for (int row = 0; row < 5; row++) {
+        for (int col = 0; col < 6; col++) {
+            // Try horizontal move if valid.
+            if (col < 5 && hor[row][col] == 0) {
+                int tempHor[5][6], tempVer[5][6];
+                char tempBoxes[4][5];
+                copy_state(hor, ver, boxes, tempHor, tempVer, tempBoxes);
+                
+                int oldScore = countBoxes(bot, tempBoxes);
+                tempHor[row][col] = 1;
+                checksquare(row, col, bot, tempHor, tempVer, tempBoxes);
+                int newScore = countBoxes(bot, tempBoxes);
+                int gain = newScore - oldScore;
+                
+                if (gain > 0) {
+                    moveFound = 1;
+                    // Earned boxes give an extra turn: simulate further chain moves.
+                    int chainGain = gain + simulate_chain(tempHor, tempVer, tempBoxes, bot);
+                    if (chainGain > maxChain)
+                        maxChain = chainGain;
+                }
+            }
+            // Try vertical move if valid.
+            if (row < 4 && ver[row][col] == 0) {
+                int tempHor[5][6], tempVer[5][6];
+                char tempBoxes[4][5];
+                copy_state(hor, ver, boxes, tempHor, tempVer, tempBoxes);
+                
+                int oldScore = countBoxes(bot, tempBoxes);
+                tempVer[row][col] = 1;
+                checksquare(row, col, bot, tempHor, tempVer, tempBoxes);
+                int newScore = countBoxes(bot, tempBoxes);
+                int gain = newScore - oldScore;
+                
+                if (gain > 0) {
+                    moveFound = 1;
+                    int chainGain = gain + simulate_chain(tempHor, tempVer, tempBoxes, bot);
+                    if (chainGain > maxChain)
+                        maxChain = chainGain;
+                }
+            }
+        }
+    }
+    
+    // If no move yields a box immediately, no chain exists.
+    if (!moveFound)
+        return 0;
+    return maxChain;
+}
+
+// Modified medium bot: tries to maximize chain moves.
+int medium_bot(int hor[5][6], int ver[5][6], char boxes[4][5], char bot) {
+    int bestChain = 0;
+    int bestRow = -1, bestCol = -1, bestIsHorizontal = -1;
+    
+    // Evaluate all valid moves.
+    for (int row = 0; row < 5; row++) {
+        for (int col = 0; col < 6; col++) {
+            // Try horizontal moves.
+            if (col < 5 && hor[row][col] == 0) {
+                int tempHor[5][6], tempVer[5][6];
+                char tempBoxes[4][5];
+                copy_state(hor, ver, boxes, tempHor, tempVer, tempBoxes);
+                
+                int oldScore = countBoxes(bot, tempBoxes);
+                tempHor[row][col] = 1;
+                checksquare(row, col, bot, tempHor, tempVer, tempBoxes);
+                int newScore = countBoxes(bot, tempBoxes);
+                int gain = newScore - oldScore;
+                
+                // If this move gains boxes, simulate chain moves.
+                if (gain > 0) {
+                    int chainGain = gain + simulate_chain(tempHor, tempVer, tempBoxes, bot);
+                    if (chainGain > bestChain) {
+                        bestChain = chainGain;
+                        bestRow = row;
+                        bestCol = col;
+                        bestIsHorizontal = 1;
+                    }
+                }
+            }
+            
+            // Try vertical moves.
+            if (row < 4 && ver[row][col] == 0) {
+                int tempHor[5][6], tempVer[5][6];
+                char tempBoxes[4][5];
+                copy_state(hor, ver, boxes, tempHor, tempVer, tempBoxes);
+                
+                int oldScore = countBoxes(bot, tempBoxes);
+                tempVer[row][col] = 1;
+                checksquare(row, col, bot, tempHor, tempVer, tempBoxes);
+                int newScore = countBoxes(bot, tempBoxes);
+                int gain = newScore - oldScore;
+                
+                if (gain > 0) {
+                    int chainGain = gain + simulate_chain(tempHor, tempVer, tempBoxes, bot);
+                    if (chainGain > bestChain) {
+                        bestChain = chainGain;
+                        bestRow = row;
+                        bestCol = col;
+                        bestIsHorizontal = 0;
+                    }
+                }
+            }
+        }
+    }
+    
+    // If a move with a positive chain gain was found, execute it.
+    if (bestChain > 0) {
+        int oldScore = countBoxes(bot, boxes);
+        if (bestIsHorizontal) {
+            hor[bestRow][bestCol] = 1;
+            checksquare(bestRow, bestCol, bot, hor, ver, boxes);
+            printf("Bot chose: Horizontal at (%d, %d)", bestRow, bestCol);
+        } else {
+            ver[bestRow][bestCol] = 1;
+            checksquare(bestRow, bestCol, bot, hor, ver, boxes);
+            printf("Bot chose: Vertical at (%d, %d)", bestRow, bestCol);
+        }
+        int newScore = countBoxes(bot, boxes);
+        return newScore - oldScore;
+    }
+    
+    // Otherwise, fallback to a random move (like the easy bot).
+    int row, col, isHorizontal;
+    int valid_move = 0;
+    while (!valid_move) {
+        row = rand() % 5;
+        col = rand() % 6;
+        isHorizontal = rand() % 2;
+        if (isHorizontal && col < 5 && hor[row][col] == 0) {
+            int oldScore = countBoxes(bot, boxes);
+            hor[row][col] = 1;
+            checksquare(row, col, bot, hor, ver, boxes);
+            int newScore = countBoxes(bot, boxes);
+            valid_move = 1;
+            printf("Bot chose: Horizontal at (%d, %d)\n", row, col);
+            return newScore - oldScore;
+        }
+        else if (!isHorizontal && row < 4 && ver[row][col] == 0) {
+            int oldScore = countBoxes(bot, boxes);
+            ver[row][col] = 1;
+            checksquare(row, col, bot, hor, ver, boxes);
+            int newScore = countBoxes(bot, boxes);
+            valid_move = 1;
+            printf("Bot chose: Vertical at (%d, %d)\n", row, col);
+            return newScore - oldScore;
+        }
+    }
+    return 0; // Should never reach here.
 }
